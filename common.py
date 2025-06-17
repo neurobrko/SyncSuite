74469351da237e222fd3c674486dead8fbd84499
Marek Paulik <marpauli@cisco.com>
2025-06-17 10:45:18 +0200

    indented logger added

--- common.py
+++ common.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import inspect
 import logging
 from pathlib import Path
 from time import strftime
@@ -48,10 +49,63 @@ info_handler.setFormatter(info_formatter)
 dev_handler = logging.FileHandler(log_path / dev_filename)
 dev_handler.addFilter(lambda record: record.levelno != logging.INFO)
 dev_formatter = logging.Formatter(
-    "%(asctime)s | %(levelname)s: [%(filename)s:%(lineno)d] %(message)s",
+    "%(asctime)s | %(levelname)s [%(filename)s:%(lineno)d]: %(message)s",
     datefmt="%H:%M:%S",
 )
 dev_handler.setFormatter(dev_formatter)
 # Add handlers to the logger
 LOGGER.addHandler(info_handler)
 LOGGER.addHandler(dev_handler)
+
+
+class IndentedLogger:
+    def __init__(self, logger):
+        self.logger = logger
+        self.log_prefix = "HH:MM:SS |"
+
+    @staticmethod
+    def _get_caller_info():
+        caller_frame = inspect.currentframe().f_back
+        filename = Path(caller_frame.f_code.co_filename).name
+        return filename, caller_frame.f_lineno
+
+    def _format_message(self, msg: list, prefix: str, level: str) -> str:
+        print(level)
+        if level == "INFO":
+            log_prefix = ""
+        else:
+            filename, lineno = self._get_caller_info()
+            log_prefix = f"{self.log_prefix} {level.upper()} [{filename}:{lineno}]: "
+        if prefix:
+            log_prefix += prefix
+            indented_message = f"{prefix} " + ("\n" + " " * (len(log_prefix) + 1)).join(
+                msg
+            )
+        else:
+            indented_message = ("\n" + " " * (len(log_prefix))).join(msg)
+        return indented_message
+
+    def log(self, level: str, msg: list, m_prefix: str | None = None):
+        indented_message = self._format_message(msg, m_prefix, level)
+        log_method = getattr(self.logger, level.lower(), None)
+        if log_method:
+            log_method(indented_message)
+
+    def debug(self, msg: list, m_prefix: str | None = None):
+        self.log("DEBUG", msg, m_prefix)
+
+    def info(self, msg: list, m_prefix: str | None = None):
+        self.log("INFO", msg, m_prefix)
+
+    def warning(self, msg: list, m_prefix: str | None = None):
+        self.log("WARNING", msg, m_prefix)
+
+    def error(self, msg: list, m_prefix: str | None = None):
+        self.log("ERROR", msg, m_prefix)
+
+    def critical(self, msg: list, m_prefix: str | None = None):
+        self.log("CRITICAL", msg, m_prefix)
+
+
+# Create an instance of IndentedLogger
+I_LOGGER = IndentedLogger(LOGGER)
--- log_cleanup.py
+++ log_cleanup.py
@@ -3,13 +3,16 @@
 from pathlib import Path
 import time
 
-from common import LOGGER
+from common import I_LOGGER, LOGGER
 
 """
 To perform log cleanup, run this script manually or create a cron job to run it periodically.
 You can also run it with every GUI start. Usually, GUI is started once a day or less often,
 than main script, so it is a good place to run this script.
 """
+
+INFO_LOG_PERSISTENCE = 5  # Number of days to keep runtime log files
+DEV_LOG_PERSISTENCE = 30  # Number of days to keep dev log files
 
 
 def log_cleanup():
@@ -19,21 +22,21 @@ def log_cleanup():
         LOGGER.warning(f"Log directory does not exist: {log_dir}")
         exit(1)
 
-    info_log_persistence = 5  # Number of days to keep runtime log files
-    dev_log_persistence = 30  # Number of days to keep dev log files
     now = time.time()
 
-    deleted = 0
+    deleted = []
     for file in log_dir.iterdir():
         if file.is_file():
             persistence = (
-                dev_log_persistence if "r2r-dev" in file.name else info_log_persistence
+                DEV_LOG_PERSISTENCE if "r2r-dev" in file.name else INFO_LOG_PERSISTENCE
             )
             if now - file.stat().st_mtime > persistence * 86400:
                 file.unlink()
-                deleted += 1
-                LOGGER.warning(f"Deleted: {file.name}")
-    if not deleted:
+                deleted.append(file)
+    if deleted:
+        plural = "s" if len(deleted) > 1 else ""
+        I_LOGGER.warning(deleted, f"Deleted {len(deleted)} log file{plural}:")
+    else:
         LOGGER.warning("No logs were deleted.")
 
 
